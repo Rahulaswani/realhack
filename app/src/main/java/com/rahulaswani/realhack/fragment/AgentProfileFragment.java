dca91135fc4218a7719943542508ec1dbb619ccf
Rahul Aswani <aswani@olacabs.com>
2015-04-26 01:15:37 +0530

    more files

---
+++ app/src/main/java/com/rahulaswani/realhack/fragment/AgentProfileFragment.java
@@ -0,0 +1,35 @@
+package com.rahulaswani.realhack.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.Locale;
+
+/**
+ * Created by rahul on 26/04/15.
+ */
+public class AgentProfileFragment extends Fragment {
+    public static final String ARG_PLANET_NUMBER = "planet_number";
+
+    public AgentProfileFragment() {
+        // Empty constructor required for fragment subclasses
+    }
+
+    /*@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_planet, container, false);
+        int i = getArguments().getInt(ARG_PLANET_NUMBER);
+        String planet = getResources().getStringArray(R.array.planets_array)[i];
+
+        int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
+                "drawable", getActivity().getPackageName());
+        ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
+        getActivity().setTitle(planet);
+        return rootView;
+    }*/
+}
